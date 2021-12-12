e2466d0074f26889a303ef040f7ce9826d6875cc
ramonnavarrete0831 <96016362+ramonnavarrete0831@users.noreply.github.com>
2021-12-12 16:50:26 -0600

    Delete app.config.ts

--- src/config/app.config.ts
+++
@@ -1,8 +0,0 @@
-/*CLAVE SECRETA PARA ENCRIPTAR LOS JWT*/
-
-export const PLATAFORM_NAME="Pasteleritos";
-
-export const JWT_SECRET = "3927437f-83b3-4cc6-8f04-524c091ea59c";
-
-export const SITE_URI = "https://www.pasteleritos.com.mx";
-
