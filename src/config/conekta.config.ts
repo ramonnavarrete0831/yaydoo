a211929c00148d0e8457a06db53d45e8e15b70f9
ramonnavarrete0831 <96016362+ramonnavarrete0831@users.noreply.github.com>
2021-12-12 11:32:50 -0600

    Update conekta.config.ts

--- src/config/conekta.config.ts
+++ src/config/conekta.config.ts
@@ -1,15 +1,4 @@
 /* DESARROLLO */
 export const CONEKTA_CONFIG = {
-  KEY: "key_HoVeqrTjzgjBG1zxBjoC3g",
+  KEY: "",
 };
-
-/* PRODUCCION */
-/*
-export const CONEKTA_CONFIG = {
-    key: "key_D4G5PjBBkD8KyAH2h86EQQ",
-};
-
-user : elrepostero@pasteleritos.com.mx
-password : N@varret3
-uri : https://conekta.com/
-*/
